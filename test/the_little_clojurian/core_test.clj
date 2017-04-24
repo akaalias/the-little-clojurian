31dc3df6ee5f7ae5ae35959d335e5c3f081b8259
Alexis Rondeau <alexis.rondeau@gmail.com>
2017-04-24 15:25:11 +0200

    Removes core_test.clj

--- test/the_little_clojurian/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns the-little-clojurian.core-test
-  (:require [clojure.test :refer :all]
-            [the-little-clojurian.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
