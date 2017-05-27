b47be3ac9b6a00a6102eaeac59ab2bbe579a8375
Alexis Rondeau <alexis.rondeau@gmail.com>
2017-05-27 15:25:34 +0200

    Adds leinfmt to ship.sh

--- ship.sh
+++ ship.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! lein cljfmt check; then
+  # whitespace needs fixing
+  echo "There are issues with whitespace. Run lein cljfmt fix, then add the changes and make your commit"
+fi
+
 if git diff-index --quiet HEAD --; then
   # no changes
   lein clean && lein test && git push origin master
